6a30a697151eab3b06b139ac4ef28727e6888409
Bouteilloux <Bouteilloux@Bouteilloux-PC>
2010-12-26 19:56:15 +0100

    Voila l'interface serveur actuel et l'interface client à remplir avec des recommandations

---
+++ src/InterfaceClient.java
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * @author Bouteilloux
+ * Donc ça c'est à toi de remplir Gilles
+ * Il faut qu'il y ait une fonction pour que tu puisses avoir
+ * des mises à jour pour ton innterface.
+ * L'autre groupe avait proposer pour qu'on soit sur les mêmes
+ * type de donnés d'utiliser:
+ * 
+ * List<Object[]> listeInfoJoueur;
+ *  List<Object[]> listeInfoJoueur;
+ *  Object[] mesInfos = {(String Pseudo, int solde, int mise ,int statut};
+ */
+
+public class InterfaceClient 
+{
+
+}
